e729473f1539b5feeabef829516d4188271dff05
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-27 10:36:52 +0100

    Navigation without links so far; added expand button and sub-list

--- src/Header/Navigation/index.js
+++ src/Header/Navigation/index.js
@@ -1,12 +1,26 @@
-import { Item, Wrapper } from "./styled";
+import { useState } from "react";
+import { Button, ExpandedItem, ExpandedList, StyledItem, Wrapper } from "./styled";
+import ChevronDownIcon from "./ChevronDownIcon";
 
 export default () => {
+    const [expand, setExpand] = useState(false);
+    const toggleExpand = () => setExpand(!expand);
+
     return (
         <Wrapper>
-            <Item>projekty/realizacje</Item>
-            <Item>nagrody/publikacje</Item>
-            <Item>o nas</Item>
-            <Item>kontakt</Item>
+            <StyledItem>projekty/realizacje<Button expanded={expand} onClick={toggleExpand}><ChevronDownIcon /></Button></StyledItem>
+            { 
+            expand && 
+            <ExpandedList>
+            <ExpandedItem>— wielorodzinne</ExpandedItem>
+            <ExpandedItem>— jednorodzinne</ExpandedItem>
+            <ExpandedItem>— biurowce</ExpandedItem>
+            <ExpandedItem>— hotele</ExpandedItem>
+            <ExpandedItem>— wnętrza komercyjne</ExpandedItem>
+            </ExpandedList>}
+            <StyledItem>nagrody/publikacje</StyledItem>
+            <StyledItem>o nas</StyledItem>
+            <StyledItem>kontakt</StyledItem>
         </Wrapper>
     )
 };
--- src/Header/Navigation/styled.js
+++ src/Header/Navigation/styled.js
@@ -1,25 +1,45 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.ul`
-display: block;
-width: 220px;
-margin: 0 0 48px 0;
-padding: 0;
+export const Wrapper = styled.ul`   
+    display: block;
+    width: 220px;
+    margin: 0 0 48px 0;
+    padding: 0;
+    color: #999;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+`
 
-height: 400px;
-background-color: blanchedalmond;
+export const StyledItem = styled.li`
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 0;
+    border-top: 1px solid rgba(0,0,0,.06);
+    text-align: left;
+    text-decoration: none;
+    list-style-type: none;
+`
 
-color: rgb(119, 119, 119);
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: 24px;
-`;
+export const Button = styled.button`
+    width: 24x;
+    height: 24px;
+    padding: 0;
+    margin-right: 12px;
+    border: none;
+    background: none;
 
-export const Item = styled.li`
-padding: 12px 0;
-border-bottom: 1px solid rgb(153, 153, 153);
-text-align: left;
-text-decoration: none;
-list-style-type: none;
+    ${({ expanded }) => expanded && `
+        transform: rotate(180deg)
+    `};
+`
+
+export const ExpandedList = styled.ul`
+    padding: 0;
+`
+
+export const ExpandedItem = styled(StyledItem)`
+    justify-content: left;
+    padding-left: 16px;
 `
