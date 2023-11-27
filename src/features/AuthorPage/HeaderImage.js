98ced9c107adf906f93cf79b1662503040fc0f8f
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-27 11:11:37 +0100

    added image and content to Authors Page

---
+++ src/features/AuthorPage/HeaderImage.js
@@ -0,0 +1,13 @@
+import headerImage from './img/0-mbednarz.jpg';
+import { StyledImage } from '../styled';
+
+export default () => {
+    return (
+    <StyledImage 
+        src={headerImage} 
+        alt='black and white two-third portrait 
+            of a white male with glasses and facail hair 
+            on a light drape background'
+        />
+    )
+}
--- src/features/AuthorPage/index.js
+++ src/features/AuthorPage/index.js
@@ -1,13 +1,27 @@
+import HeaderImage from "./HeaderImage";
 import { Container, Link, StyledParagraph } from "../styled";
 
 export default () => (
     <Container>
+        <HeaderImage />
         <StyledParagraph>
             {`Hi! My name is `}
             <Link href="https://github.com/bedicooper">Mateusz Bednarz</Link>
-            {`. This is Authors Page placeholder.`} 
-        </StyledParagraph>       
+            {`. Thanks for stopping by!`}
+        </StyledParagraph>
+        <StyledParagraph>
+            {`I'm currently studing to become Frontend Developer. 
+                I have created this mockup for practice
+                and as a showcase of my skillset.
+                I tried to reproduce as much form original site as possible,
+                though not all original features are implemented yet.`}
+        </StyledParagraph>
+        <StyledParagraph>
+            {`I have chosen that particular website, 
+                because as an Architect by trade, 
+                I have been cooperating with studio Kosmos for the last 10 years
+                on various designs.
+            `}
+        </StyledParagraph>
     </Container>
 );
-
-
