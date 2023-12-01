96ceef120fbb25e3f25d9eff1c66db1c4d20e4ef
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-12-01 14:47:58 +0100

    added array with projects info

---
+++ src/features/Portfolio/projects.js
@@ -0,0 +1,13 @@
+export default [
+    {title: 'mestwina', thumbnail:'mestwina-front', type:'hotel', id:11},
+    {title: 'DĄBROWSKIEGO', thumbnail:'dabrowskiego-front', type:'hotel', id:10},
+    {title: 'LUBICZ',thumbnail:'lubicz-front', type:'biuro', id:9},
+    {title: 'RAKOWIECKA', thumbnail:'rakowiecka-front', type:'wielo', id:8},
+    {title: 'KOMANDORSKA', thumbnail:'komandorska-front', type:'biuro', id:7},
+    {title: 'POBORZAŃSKA', thumbnail:'poborzanska-front', type:'wielo', id:6},
+    {title: 'MIŃSKA', thumbnail:'minska-front', type:'wielo', id:5},
+    {title: 'ratibora', thumbnail:'ratibora-front', type:'hotel', id:4},
+    {title: 'izabelin', thumbnail:'izabelin-front', type:'jedno', id:3},
+    {title: 'dachowa', thumbnail:'dachowa-front', type:'jedno', id:2},
+    {title: 'piłsudskiego', thumbnail:'pilsudskiego-front', type:'biuro', id:1},
+];
